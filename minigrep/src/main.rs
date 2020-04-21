227af390eb01e32c163a761c49ce5eeeaa7da448
Elad Matia <elad.matia@gmail.com>
2020-04-21 22:26:22 +0300

    Refactored all the code according to rust guidelines

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,20 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3{
-        panic!("Insufficient arguments")
-    }
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Query: {}", config.query);
+    println!("Filename: {}", config.filename);
 
-    println!("Query: {}", query);
-    println!("Filename: {}", filename);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
