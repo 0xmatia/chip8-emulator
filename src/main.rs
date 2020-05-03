11fc70cd93dd8901c26bb34c66cea104a1277a33
Elad Matia <elad.matia@gmail.com>
2020-05-03 12:18:44 +0300

    removed minigrep

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     // create chip8 instance
     let mut chip8 = Chip8::new();
 
-    if let Err(e) = chip8.load_rom("Roms\\BRIX") {
+    if let Err(e) = chip8.load_rom("Roms\\PONG2") {
         eprintln!("Couldn't load ROM: {}", e);
 
         process::exit(1);
@@ -32,7 +32,7 @@ fn main() {
                         print!("*");
                     }
                     else {
-                        print!("-");
+                        print!(" ");
                     }
                 }
                 println!();
